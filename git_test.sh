5c5b13f3f8f746037c862376a21f62143e9751c5
yoda83 <bjones1831@gmail.com>
2016-09-30 12:55:21 -0500

    Update git_test.sh

--- git_test.sh
+++ git_test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "Making an update to test the remote execution (static, NOT from github)." >> /home/jenkins/test.txt
+echo "Testing remote Salt minion installation (static, NOT from github)." >> /home/jenkins/test.txt
